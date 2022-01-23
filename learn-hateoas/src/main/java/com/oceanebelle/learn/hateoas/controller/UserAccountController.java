7329b17d6cde0b58a936eff3c2986412b9071f00
oceanebelle <oceanebelle@users.noreply.github.com>
2022-01-23 13:13:58 +0000

    add unit test to the controller using restassured

--- learn-hateoas/src/main/java/com/oceanebelle/learn/hateoas/controller/UserAccountController.java
+++ learn-hateoas/src/main/java/com/oceanebelle/learn/hateoas/controller/UserAccountController.java
@@ -1,7 +1,5 @@
 package com.oceanebelle.learn.hateoas.controller;
 
-import brave.Tracer;
-import brave.propagation.CurrentTraceContext;
 import com.oceanebelle.learn.hateoas.controller.config.RequestId;
 import com.oceanebelle.learn.hateoas.controller.dto.UserDTO;
 import com.oceanebelle.learn.hateoas.controller.hateoas.model.User;
@@ -17,31 +15,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 
-import static com.oceanebelle.learn.logging.LogMessageFactory.failAccess;
+import static com.oceanebelle.learn.logging.LogMessageFactory.endAccess;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 // TODO: Add validation to the dto
 @Log4j2
 @RestController
-public class UserAccountApi {
+public class UserAccountController {
 
-    private final Tracer tracer;
+//    private final Tracer tracer;
     private final UserService userService;
-    private final CurrentTraceContext currentTraceContext;
+//    private final CurrentTraceContext currentTraceContext;
 
-    public UserAccountApi(UserService userService, Tracer tracer, CurrentTraceContext currentTraceContext) {
+    public UserAccountController(UserService userService) {
         this.userService = userService;
-        this.tracer = tracer;
-        this.currentTraceContext = currentTraceContext;
     }
 
 
@@ -49,18 +40,22 @@ public class UserAccountApi {
     List<User> getAllUsers(RequestId requestId) {
         log.info(LogMessageFactory.startAccess("userApi").method().with(requestId));
 
-        try {
-            // Test if the trace is propagated
-            var future = CompletableFuture.runAsync(currentTraceContext.wrap(() -> log.info("Task ran in separate thread")));
-            future.get(1000, TimeUnit.MICROSECONDS);
-        } catch (Exception ex) {
-            log.error(failAccess("userApi").method(), ex);
-        }
+//        try {
+//            // Test if the trace is propagated
+//            var future = CompletableFuture.runAsync(currentTraceContext.wrap(() -> log.info("Task ran in separate thread")));
+//            future.get(1000, TimeUnit.MICROSECONDS);
+//        } catch (Exception ex) {
+//            log.error(failAccess("userApi").method(), ex);
+//        }
 
-        return userService.getUsers().stream()
+        var result = userService.getUsers().stream()
                 .map(u -> new User(u.getName(), u.getId()))
-                .map(u -> u.add(linkTo(methodOn(UserAccountApi.class).getUser(u.getId())).withSelfRel()))
+                .map(u -> u.add(linkTo(methodOn(UserAccountController.class).getUser(u.getId())).withSelfRel()))
                 .collect(Collectors.toList());
+
+        log.info(endAccess("userApi").method().kv("size", result.size()));
+
+        return result;
     }
 
     @PostMapping("/users")
@@ -72,14 +67,14 @@ public class UserAccountApi {
 
     @GetMapping("/users/{id}")
     ResponseEntity<User> getUser(@PathVariable("id") String id) {
-        log.info(LogMessageFactory.startAccess("userApi").method().kv("requestId", id));
+        log.info(LogMessageFactory.startAccess("userApi").method().kv("id", id));
         var result = userService.getUser(id)
                 .map(u -> new User(u.getName(), u.getId()))
-                .map(u -> u.add(linkTo(methodOn(UserAccountApi.class).getAllUsers(null)).withRel("all")))
-                .map(u -> u.add(linkTo(methodOn(UserAccountApi.class).getUser(u.getId())).withSelfRel()));
+                .map(u -> u.add(linkTo(methodOn(UserAccountController.class).getAllUsers(null)).withRel("all")))
+                .map(u -> u.add(linkTo(methodOn(UserAccountController.class).getUser(u.getId())).withSelfRel()));
 
 
-        log.info(LogMessageFactory.endAccess("userApi").method().kv("requestId", id).kv("found", result.isPresent()));
+        log.info(LogMessageFactory.endAccess("userApi").method().kv("id", id).kv("found", result.isPresent()));
         if (result.isPresent()) {
             return ResponseEntity.ok(result.get());
         } else {
--- learn-hateoas/src/main/java/com/oceanebelle/learn/hateoas/controller/config/RequestId.java
+++ learn-hateoas/src/main/java/com/oceanebelle/learn/hateoas/controller/config/RequestId.java
@@ -2,11 +2,15 @@ package com.oceanebelle.learn.hateoas.controller.config;
 
 import com.oceanebelle.learn.logging.LogMessage;
 import com.oceanebelle.learn.logging.LogMessageVisitor;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RequestId implements LogMessageVisitor {
     private String requestId;
 
