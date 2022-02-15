7639d8f3b23849f4957d86619013102416ada409
oceanebelle <oceanebelle@users.noreply.github.com>
2022-02-15 23:37:30 +0000

    add tracing to interactive commands

--- learn-shell-noninteractive/src/main/java/com/oceanebelle/learn/shell/HelloWorldCommand.java
+++ learn-shell-noninteractive/src/main/java/com/oceanebelle/learn/shell/HelloWorldCommand.java
@@ -2,15 +2,22 @@ package com.oceanebelle.learn.shell;
 
 import com.oceanebelle.learn.logging.LogMessageFactory;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
 @Log4j2
 @ShellComponent
 public class HelloWorldCommand {
+
+    @NewSpan
     @ShellMethod("Hello World")
     public String hello() {
-        log.info(LogMessageFactory.startAction("SHELL_COMMAND").method());
-        return "Hello World";
+        log.info(LogMessageFactory.startAction("RUN_COMMAND").method());
+        try {
+            return "Hello World";
+        } finally {
+            log.info(LogMessageFactory.endAction("RUN_COMMAND").method());
+        }
     }
 }
--- learn-shell-noninteractive/src/main/java/com/oceanebelle/learn/shell/ShellNonInteractive.java
+++ learn-shell-noninteractive/src/main/java/com/oceanebelle/learn/shell/ShellNonInteractive.java
@@ -1,8 +1,11 @@
 package com.oceanebelle.learn.shell;
 
+import com.oceanebelle.learn.logging.LogMessageFactory;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.shell.Shell;
@@ -13,19 +16,23 @@ import org.springframework.stereotype.Component;
 @Component
 @Order(InteractiveShellApplicationRunner.PRECEDENCE - 100)
 @AllArgsConstructor
+@Log4j2
 public class ShellNonInteractive implements ApplicationRunner {
 
     private final Shell shell;
     private final ConfigurableEnvironment environment;
     private final DefaultResultHandler resultHandler;
 
+    @NewSpan
     @Override
     public void run(ApplicationArguments args) throws Exception {
         if (args.getSourceArgs().length > 0) {
             InteractiveShellApplicationRunner.disable(environment);
             var input = String.join(" ", args.getSourceArgs());
+            log.info(LogMessageFactory.startAction("NON_INTERACTIVE").kv("command", input));
             resultHandler.handleResult(shell.evaluate(() -> input));
             shell.evaluate(() -> "exit");
+            log.info(LogMessageFactory.endAction("NON_INTERACTIVE").kv("command", input));
         }
     }
 }
